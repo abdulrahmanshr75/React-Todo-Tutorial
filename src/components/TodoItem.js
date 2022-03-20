2d06c99770f10300aaa964724b132e9ba2ae1b6f
abdulrahmanshr75 <94563329+abdulrahmanshr75@users.noreply.github.com>
2022-03-20 14:11:54 +0200

    setup

---
+++ src/components/TodoItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class TodoItem extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <li>
+          <input
+            type="checkbox"
+            checked={this.props.todo.completed}
+            onChange={() => this.props.handleChange(this.props.todo.id)}
+          />
+          {this.props.todo.title}
+        </li>
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default TodoItem;
