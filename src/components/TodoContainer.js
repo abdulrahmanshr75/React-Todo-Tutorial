610a2e2431944a2662acf7db2e423b97d71bee5c
abdulrahmanshr75 <94563329+abdulrahmanshr75@users.noreply.github.com>
2022-03-21 11:18:33 +0200

    finish the class component lesson

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -5,33 +5,7 @@ import InputTodo from "./InputTodo";
 import { v4 as uuidv4 } from "uuid";
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false,
-      },
-    ],
-  };
-  setUpdate = (updatedTitle, id) => {
-    this.setState({
-      todos: this.state.todos.map((todo) => {
-        if (todo.id === id) {
-          todo.title = updatedTitle;
-        }
-        return todo;
-      }),
-    });
+    todos: [],
   };
   handleChange = (id) => {
     this.setState((prevState) => ({
@@ -65,7 +39,31 @@ class TodoContainer extends React.Component {
       todos: [...this.state.todos, newTodo],
     });
   };
-
+  setUpdate = (updatedTitle, id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = updatedTitle;
+        }
+        return todo;
+      }),
+    });
+  };
+  componentDidMount() {
+    const temp = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(temp);
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos,
+      });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos);
+      localStorage.setItem("todos", temp);
+    }
+  }
   render() {
     return (
       <div className="container">
